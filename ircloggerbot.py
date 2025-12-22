414187afe17dadd4db67ec58005a65d6164992d9
scorchinbeans <scorchinsriracha@gmx.com>
2025-12-22 13:57:07 -0500

    Update ircloggerbot.py to remove example comments
    Removed the example bot description and commands.

--- ircloggerbot.py
+++ ircloggerbot.py
@@ -1,26 +1,8 @@
 #! /usr/bin/env python
 # (C) Paulus Madison Hay
 
-"""A simple example bot.
-
-This is an example bot that uses the SingleServerIRCBot class from
-irc.bot.  The bot enters a channel and listens for commands in
-private messages and channel traffic.  Commands in channel messages
-are given by prefixing the text by the bot name followed by a colon.
-It also responds to DCC CHAT invitations and echos data sent in such
-sessions.
-
-The known commands are:
-
-    stats -- Prints some channel information.
-
-    disconnect -- Disconnect the bot.  The bot will try to reconnect
-                  after 60 seconds.
-
-    die -- Let the bot cease to exist.
-
-    dcc -- Let the bot invite you to a DCC CHAT connection.
-"""
+# Logs irc traffic on every channel
+# it is commanded to join.
 
 import irc.bot
 import irc.strings
